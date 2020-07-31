74e12f8b904f68223e45cedcee657f18cfa3cc7e
Ahmet Alp <ahmet.alp77@gmail.com>
2020-08-01 00:09:11 +0100

    Delete UserRepository.java

--- src/main/java/com/mifinity/card/service/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mifinity.card.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mifinity.card.model.User;
-
-public interface UserRepository extends JpaRepository<User, String> {
-    User findByUsername(String username);
-}
